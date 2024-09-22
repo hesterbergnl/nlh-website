542594a6060f0cf40ed06f2c9f3743bf3025ec36
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2024-09-22 22:38:41 +0200

    Abstracted functionality of blog route handling to blog service. Added some validation and typing

--- nlh-website-backend/todo.ts
+++ nlh-website-backend/todo.ts
@@ -1,7 +1,8 @@
+//TODO fix git branches issue
 //TODO setup basic structure for user routes
 //TODO separate out functionality to services
 //TODO setup proper typing and validation for routes/services
-//TODO reasearch best way to host / display photos - static from server or CDN or firebase etc
+//TODO research best way to host / display photos - static from server or CDN or firebase etc
 //TODO implement photo uploads functionality
 //TODO add page analytics
 //TODO add links to personal pages
